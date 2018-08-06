14ce3a6f74c357f953361b354e54c978879d85a5
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2018-08-06 20:31:04 -0300

    added wherefore-art-thou.js

---
+++ javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/wherefore-art-thou.js
@@ -0,0 +1,28 @@
+//=======================================================================================================================================
+// Intermediate Algorithm Scripting: Wherefore art thou
+//=======================================================================================================================================
+
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+
+  for(let i in collection){
+    var iHasAll = true;
+    for(let key in source){
+      iHasAll = iHasAll && collection[i][key] === source[key];
+    }
+    if (iHasAll){
+      arr.push(collection[i]);
+    }
+  } 
+
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+//=======================================================================================================================================
+//
+//=======================================================================================================================================
